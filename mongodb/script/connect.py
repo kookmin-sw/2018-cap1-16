07a7bc07d2b988c5065941707b649f5c2675349a
Junnyung-Heo <hurjn96@naver.com>
2018-03-12 18:36:34 +0900

    modify connect.py to class structure

--- mongodb/script/connect.py
+++ mongodb/script/connect.py
@@ -2,55 +2,55 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 from pymongo.errors import OperationFailure
 import os, time, re, getpass
+class MyMongoClient:
+	def __init__(self,ip,port,db,collection):
+		self.client = self.connect_db(ip,port)
+		self.check_connection()
+		self.db = self.get_db(db)
+		self.collection = self.get_collection(collection)
 
-def connect_db(ip,port):
-	client = MongoClient('mongodb://%s:%d' %(ip,port), serverSelectionTimeoutMS = 1500)
-	
-	return client
+	def connect_db(self,ip,port):
+		client = MongoClient('mongodb://%s:%d' %(ip,port), serverSelectionTimeoutMS = 1500)
+		
+		return client
 
-def check_connection():
-	try : 
-		client.admin.command('ismaster')
-	except ConnectionFailure:
-		print("Server is not available")
-		exit()
-	
+	def check_connection(self):
+		try : 
+			self.client.admin.command('ismaster')
+		except ConnectionFailure:
+			print("Server is not available")
+			exit()
+		
 
-def get_db(client,db_name):
-	while(True):
-		user_id , user_pwd = get_authenticate()
-		db = client[db_name]
-		try:
-			db.authenticate(user_id,user_pwd)
-		except OperationFailure:
-			print("Failed to login")
-			continue
+	def get_db(self,db_name):
+		while(True):
+			user_id , user_pwd = self.get_authenticate()
+			db = self.client[db_name]
+			try:
+				db.authenticate(user_id,user_pwd)
+			except OperationFailure:
+				print("Failed to login")
+				continue
 
-		print('Login successfully')
-		break
+			print('Login successfully')
+			break
 
-	return db
+		return db
 
-def get_authenticate():
-	user_id = ' '
-	input_id_first = True
-	while not re.match("^[A-Za-z0-9]+$", user_id):
-		if not input_id_first:
-			print("Error! you can use only Alphabet or Number for your id")
-		user_id = input("ID: ")
-		input_id_first = False
-			
-	user_pwd = getpass.getpass()
+	def get_authenticate(self):
+		user_id = ' '
+		input_id_first = True
+		while not re.match("^[A-Za-z0-9]+$", user_id):
+			if not input_id_first:
+				print("Error! you can use only Alphabet or Number for your id")
+			user_id = input("ID: ")
+			input_id_first = False
+				
+		user_pwd = getpass.getpass()
 
-	return user_id, user_pwd
+		return user_id, user_pwd
 
-def get_collection(db,collection_name):
-	collection = db[collection_name]
+	def get_collection(self,collection_name):
+		collection = self.db[collection_name]
 
-	return collection
-
-if __name__ == '__main__':
-	client = connect_db('localhost',27017)
-	check_connection()
-	db = get_db(client,'seclab')
-	collection = get_collection(db,'analyzed_report')
+		return collection
