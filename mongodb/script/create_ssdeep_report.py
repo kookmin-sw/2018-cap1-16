8297fadddc629566052ab7eeeaca3b006a7026c3
Junnyung-Heo <hurjn96@naver.com>
2018-03-18 11:46:45 +0900

    add 'upload_ssdeep_report' script

--- mongodb/script/create_ssdeep_report.py
+++ mongodb/script/create_ssdeep_report.py
@@ -7,10 +7,12 @@ for file in files:
 	abs_url = os.path.join(DIR,file)
 	abs_out_url = os.path.join(OUT_DIR,file.replace('.ops',"json"))
 
+	file_md5 = file.replace('.ops',"")
 	ssdeep_str = ssdeep.hash_from_file(abs_url)
 
-	out_f = open(abs_out_url,'w',encoding='utf-8')
+	out_file = open(abs_out_url,'w',encoding='utf-8')
 	report = dict()
+	report['MD5'] = file_md5
 	report['SSDeep'] = ssdeep_str
-	json.dump(report,out_f, ensure_ascii=False, indent = '\t')
+	json.dump(report, out_file, ensure_ascii=False, indent = '\t')
 
