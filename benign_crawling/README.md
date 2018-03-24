f84a9dbbb3e3aaa907ef55873783d8eb265d4d6b
Kim Young Jae <corea_kyj@naver.com>
2018-03-24 17:38:34 +0900

    Benign Crawling :: Version 2018.03.24 * exe 파일을 32bit, 64bit로 구분해서 `<md5>.vir` 형태로 저장하는 크롤러 제작 * dll 파일을 32bit, 64bit로 구분해서 `<md5>.vir` 형태로 저장하는 기능 추가 * path 확인 오류 수정

--- benign_crawling/README.md
+++ benign_crawling/README.md
@@ -3,6 +3,8 @@
 ### Update Log
 #### Version 2018.03.24
 * exe 파일을 32bit, 64bit로 구분해서 `<md5>.vir` 형태로 저장하는 크롤러 제작
+* dll 파일을 32bit, 64bit로 구분해서 `<md5>.vir` 형태로 저장하는 기능 추가
+* path 확인 오류 수정
 
 ### 사용법
-1. `run(<PATH>)` : `<PATH>` 를 기준으로 하위 폴더에 있는 모든 exe파일을 32bit 와 64bit로 구분해서 저장 한다.
+1. `run(<PATH>)` : `<PATH>` 를 기준으로 하위 폴더에 있는 모든 pe파일을 32bit 와 64bit로 구분해서 저장 한다.
--- benign_crawling/benign_crawling.py
+++ benign_crawling/benign_crawling.py
@@ -2,10 +2,10 @@ import pefile, os, shutil, hashlib
 
 import multiprocessing as mp
 
-from benign_crawling.settings import *
+from settings import *
 
 def get_file_path( root ) :
-    path_32bit_exe, path_64bit_exe = [], []
+    path_32bit_exe, path_64bit_exe, path_32bit_dll, path_64bit_dll = [], [], [], []
     for path, dirs, files in os.walk(root) :
         for file in files :
             ext = os.path.splitext(file)[-1]
@@ -24,7 +24,21 @@ def get_file_path( root ) :
                         print("Error :", full_path)
                 except :
                     pass
-    return path_32bit_exe, path_64bit_exe
+            elif ext == '.dll' :
+                try :
+                    pe = pefile.PE(full_path)
+                    machine_bit = pe.FILE_HEADER.Machine
+                    if machine_bit == 0x014c:
+                        print("32bit :", full_path)
+                        path_32bit_dll.append(full_path)
+                    elif machine_bit == 0x8664 or machine_bit == 0x0200:
+                        print("64bit :", full_path)
+                        path_64bit_dll.append(full_path)
+                    else :
+                        print("Error :", full_path)
+                except :
+                    pass
+    return path_32bit_exe, path_64bit_exe, path_32bit_dll, path_64bit_dll
 
 def get_file_md5( file_path, block_size = 8192 ) :
     hasher = hashlib.md5()
@@ -35,27 +49,53 @@ def get_file_md5( file_path, block_size = 8192 ) :
             buf = f.read(block_size)
     return hasher.hexdigest()
 
-def crawling_32bit( path_32bit ) :
+def crawling_32bit_exe( path_32bit ) :
     md5 = get_file_md5(path_32bit)
-    print(md5, path_32bit)
-    if not os.path.exists(path_32bit) :
+    target_path = os.path.join(DESTINATION_32_EXE_PATH, md5 + '.vir')
+    if not os.path.exists(target_path) :
         try :
-            shutil.copy(path_32bit, os.path.join(DESTINATION_32_PATH, md5 + '.vir'))
+            shutil.copy(path_32bit, target_path)
         except :
             pass
 
-def crawling_64bit( path_64bit ) :
+def crawling_64bit_exe( path_64bit ) :
     md5 = get_file_md5(path_64bit)
-    print(path_64bit)
-    if not os.path.exists(path_64bit) :
+    target_path = os.path.join(DESTINATION_64_EXE_PATH, md5 + '.vir')
+    if not os.path.exists(target_path) :
         try :
-            shutil.copy(path_64bit, os.path.join(DESTINATION_64_PATH, md5 + '.vir'))
+            shutil.copy(path_64bit, target_path)
+        except :
+            pass
+
+
+def crawling_32bit_dll( path_32bit ) :
+    md5 = get_file_md5(path_32bit)
+    target_path = os.path.join(DESTINATION_32_DLL_PATH, md5 + '.vir')
+    if not os.path.exists(target_path) :
+        try :
+            shutil.copy(path_32bit, target_path)
+        except :
+            pass
+
+def crawling_64bit_dll( path_64bit ) :
+    md5 = get_file_md5(path_64bit)
+    target_path = os.path.join(DESTINATION_64_DLL_PATH, md5 + '.vir')
+    if not os.path.exists(target_path) :
+        try :
+            shutil.copy(path_64bit, target_path)
         except :
             pass
 
 def run( root ) :
     mp.freeze_support()
-    path_32bit_exe, path_64bit_exe = get_file_path( root )
+    path_32bit_exe, path_64bit_exe, path_32bit_dll, path_64bit_dll = get_file_path( root )
     p = mp.Pool(os.cpu_count() // 2)
-    p.map(crawling_32bit, path_32bit_exe)
-    p.map(crawling_64bit, path_64bit_exe)
+    print("Start copy 32bit EXE : {}".format(len(path_32bit_exe)))
+    p.map(crawling_32bit_exe, path_32bit_exe)
+    print("Start copy 64bit EXE: {}".format(len(path_64bit_exe)))
+    p.map(crawling_64bit_exe, path_64bit_exe)
+    print("Start copy 32bit DLL: {}".format(len(path_32bit_dll)))
+    p.map(crawling_32bit_dll, path_32bit_dll)
+    print("Start copy 64bit DLL: {}".format(len(path_64bit_dll)))
+    p.map(crawling_64bit_dll, path_64bit_dll)
+    print("Done")
