6db60ec7d3acc3449c86d906eb32ec3feb936635
Junnyung-Heo <hurjn96@naver.com>
2018-04-08 17:39:26 +0900

    modify upload_report script

--- mongodb/script/upload_report.py
+++ mongodb/script/upload_report.py
@@ -1,7 +1,7 @@
 from connect import SeclabMongoClient
 import os,datetime,json
 
-DIRECTORY = '/home/jun/kisa_reports'
+DIRECTORY = '/home/seclab/samples_vt'
 
 def upload_report(dir_url,collection):
 	files = os.listdir(dir_url)
@@ -11,24 +11,23 @@ def upload_report(dir_url,collection):
 		absolute_url = os.path.join(dir_url,file)
 		with open(absolute_url,"r") as f:
 			data = json.load(f)
-			md5 = data['MD5']
-			magic = data['Magic']
-			sha1 = data['SHA-1']
-			sha256 = data['SHA-256']
-			filesize = int(data['File Size'])
-			detected = data['detected']
-			result = data['result']
-			ssdeep = data['SSDeep']
-			ssdeep_split = ssdeep.split(":")
-			ssdeep_chunk_size = ssdeep_split[0]
-			ssdeep_chunk = ssdeep_split[1]
-			ssdeep_double_chunk = ssdeep_split[2]
+			md5 = data['md5']
+			#magic = data['Magic']
+			sha1 = data['sha1']
+			sha256 = data['sha256']
+			#filesize = int(data['File Size'])
+			#detected = data['detected']
+			#result = data['result']
+			#ssdeep = data['SSDeep']
+			#ssdeep_split = ssdeep.split(":")
+			#ssdeep_chunk_size = ssdeep_split[0]
+			#ssdeep_chunk = ssdeep_split[1]
+			#ssdeep_double_chunk = ssdeep_split[2]
 			try:
-				collection.insert({"_id":md5,"Magic":magic,\
-					"File_Size":filesize,"SHA-1":sha1,"SHA-256":sha256,\
-					"SSDeep": ssdeep, "SSDeep_chunk_size":ssdeep_chunk_size,\
-					"SSDeep_chunk":ssdeep_chunk,"SSDeep_double_chunk": ssdeep_double_chunk,\
-					"detected":detected,"result":result,\
+				collection.insert({"_id":md5,\
+					"SHA-1":sha1,"SHA-256":sha256,\
+					#"SSDeep": ssdeep, "SSDeep_chunk_size":ssdeep_chunk_size,\
+					#"SSDeep_chunk":ssdeep_chunk,"SSDeep_double_chunk": ssdeep_double_chunk,\
 					"Uploaded_Date":datetime.datetime.now()})
 				print("%s is inserted (%d/%d) "%(md5,remain_count,file_count))
 			except:
@@ -38,6 +37,6 @@ def upload_report(dir_url,collection):
 
 
 if __name__ == '__main__':
-	client = SeclabMongoClient('localhost',27017,'seclab')
+	client = SeclabMongoClient('203.246.112.131',27017,'seclab')
 	collection = client.db['analyzed_report']
 	upload_report(DIRECTORY,collection)
