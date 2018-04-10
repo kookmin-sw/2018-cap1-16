a14cbd4ebf702d033abb5d37d8fee50cb1bf2da6
tngkr8121 <82838283@naver.com>
2018-04-10 17:55:24 +0900

    design analysis detail page

---
+++ web/static/js/progress.js
@@ -0,0 +1,7 @@
+ $(document).ready(function() {
+      $('.progress .progress-bar').css("width",
+                function() {
+                    return $(this).attr("aria-valuenow") + "%";
+                }
+        )
+    });
