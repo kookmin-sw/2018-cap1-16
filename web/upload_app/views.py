1e396dbc6b70f6c576b5688738468a02592149ec
Brilliantw <mjw95@kookmin.ac.kr>
2018-04-12 00:55:45 +0900

    static analysis interlock ann_static_analysis

--- web/upload_app/views.py
+++ web/upload_app/views.py
@@ -47,7 +47,6 @@ def detail(request, md5):
         report_form = ReportForm()
         similar_report_forms = list()
         ctx = {'report_form': report_form, 'similar_report_forms' : similar_report_forms}
-
         analysis_type = upload_file_obj.analysis_type
 
         md5_search_data = md5_search(md5)
@@ -58,6 +57,7 @@ def detail(request, md5):
             if analysis_type == 0:
                 static_analysis_data = run_static_analysis(upload_file_obj)
                 static_analysis_report_form = create_report_form(report_form,static_analysis_data)
+
                 ctx['report_form'] = static_analysis_report_form
 
         #es_ssdeep_report = es_ssdeep_search(UploadFileMeta_obj.ssdeep)
@@ -82,8 +82,8 @@ def create_report_form(report_form, search_data):
     #report_form.fields['ssdeep'].initial = search_data['SSDeep']
     report_form.fields['detected'].initial = search_data['detected']
     report_form.fields['label'].initial = search_data['label']
-    report_form.fields['uploaded_date'].initial = search_data['Uploaded_Date']
-    #report_form.fields['score'].initial = int(search_data['_score'])
+    #report_form.fields['uploaded_date'].initial = search_data['Uploaded_Date']
+    report_form.fields['score'].initial = int(search_data['score'])
 
     return report_form
 
@@ -101,17 +101,28 @@ def get_hash_str(upload_file, block_size = 8192 ) :
 def run_static_analysis(upload_file_obj):
 
     IDA_ROOT = os.path.join(settings.PROJECT_DIR,'opseq_ida')
+    ANN_ROOT = os.path.join(settings.PROJECT_DIR, 'ann_by_static')
+
     upload_file_path = os.path.join(settings.MEDIA_ROOT, upload_file_obj.upload_file.name)
 
     ops_folder_path = os.path.join(IDA_ROOT,'ops')
     ops_file_path = os.path.join(ops_folder_path,os.path.splitext(upload_file_obj.upload_file.name)[0]+'.ops')
     fh_folder_path = os.path.join(IDA_ROOT,'fh')
     fh_file_path = os.path.join(fh_folder_path,os.path.splitext(upload_file_obj.upload_file.name)[0]+'.fh')
+    static_analysis_json_path = os.path.join(os.path.join(ANN_ROOT,'test_result'),os.path.splitext(upload_file_obj.upload_file.name)[0]+'.json')
 
     cmd_run_ida_ops = 'python ' + IDA_ROOT + os.sep + 'make_idb_ops.py ' + upload_file_path
     cmd_run_ida_fh = 'python ' + IDA_ROOT + os.sep + 'make_fh.py ' + ops_file_path
     os.system(cmd_run_ida_ops)
     os.system(cmd_run_ida_fh)
+
+    cmd_run_ann = 'python ' + ANN_ROOT + os.sep + 'ann_by_static.py -t ' + fh_file_path
+    os.system(cmd_run_ann)
+
+    static_analysis_json = open(static_analysis_json_path)
+    ret_data = json.load(static_analysis_json)
+    return ret_data
+
 
 
 
