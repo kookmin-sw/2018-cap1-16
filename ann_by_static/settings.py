43cde1715359c2f68d51da643024ab040ca9d9df
Kim Young Jae <corea_kyj@naver.com>
2018-04-11 21:44:28 +0900

    학습 및 테스트 실행 코드 추가

--- ann_by_static/settings.py
+++ ann_by_static/settings.py
@@ -2,3 +2,6 @@ import os
 
 # Train Data Path
 TRAIN_DATA_PATH = os.path.normpath(os.path.abspath('./train_data'))
+
+# Test Result Path
+TEST_RESULT_PATH = os.path.normpath(os.path.abspath('./test_result'))
