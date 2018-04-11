d6bb4406b18953fd019f7f9932d43cb618741856
Kim Young Jae <corea_kyj@naver.com>
2018-04-11 21:45:41 +0900

    Fh Version 1.0 으로 테스트 하는 코드 추가

---
+++ ann_by_static/testing_model.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import numpy as np
+import pickle, os, json
+
+from settings import *
+
+# for using tensorflow as hyper parameter
+INPUT_SIZE = 12288
+OUTPUT_SIZE = 2
+
+RESULT = [ False, True ]
+
+CHECK_POINT = os.path.normpath(os.path.abspath('./static_model.ckpt'))
+
+def run( path ) :
+    with tf.device('/gpu:0'):
+        x = tf.placeholder(tf.float32, shape=[None, INPUT_SIZE])
+        y = tf.placeholder(tf.float32, shape=[None, OUTPUT_SIZE])
+
+        dense_layer_1 = tf.layers.dense(inputs=x, units=4096, activation=tf.nn.relu)
+        dense_layer_2 = tf.layers.dense(inputs=dense_layer_1, units=1024, activation=tf.nn.relu)
+        dense_layer_3 = tf.layers.dense(inputs=dense_layer_2, units=256, activation=tf.nn.relu)
+        dense_layer_4 = tf.layers.dense(inputs=dense_layer_3, units=64, activation=tf.nn.relu)
+        dense_layer_5 = tf.layers.dense(inputs=dense_layer_4, units=16, activation=tf.nn.relu)
+
+        y_ = tf.layers.dense(inputs=dense_layer_5, units=OUTPUT_SIZE)
+        y_test = tf.nn.softmax(y_)
+
+    # testing session start
+    model_saver = tf.train.Saver()
+    init = tf.global_variables_initializer()
+
+    tf_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
+    tf_config.gpu_options.allow_growth = True
+
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
+        sess.run(init)
+        model_saver.restore(sess, CHECK_POINT)
+        with open(path, 'rb') as f :
+            fh_vector = pickle.load(f)
+        result=RESULT[int(np.array(sess.run(y_test, feed_dict={x: [fh_vector]})).reshape([-1]).argmax(-1))]
+        print(result)
+        md5 = os.path.splitext(os.path.basename(path))[0]
+        result_dict = { 'md5' : md5, 'detected' : result, 'result' : 'None'}
+        with open(os.path.join(TEST_RESULT_PATH, md5 + '.json'), 'w') as f :
+            json.dump(result_dict, f)
+
+
