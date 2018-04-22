c0814277c7afecc31d759d0d82737500f016e14f
cyhan1004 <cyhan1004@naver.com>
2018-04-22 17:59:00 +0900

    bug fix 'change upload url to absolute path in upload_file.py '

--- cuckoo/scripts/upload_file.py
+++ cuckoo/scripts/upload_file.py
@@ -2,9 +2,12 @@
 '''
     Author : Chaeyeon Han
     Date created : 04/18/2018
+    Date modified : 04/22/2018
     Python version : 3.6
 
+    pip install request
     execution : "python3 upload_file.py [file directory] ([cpu count])"
+
 '''
 
 import requests, os
@@ -22,43 +25,47 @@ def get_file_name ( file_path ) :
     return os.path.basename(file_path)
 
 def send_file( file_path ) :
+    print(file_path)
     with open(file_path, 'rb') as f :
         file_name = get_file_name(file_path)
         fs = {'file' : (file_name, f)}
-        r = requests.post(REST_URL,files=fs)
-        if r.status_code == 200 :
-            print("{} is succeeded".format(file_name))
-            #global task_id
-            task_id = r.json()["task_ids"][0]
-            print(task_id)
-            status_check(task_id)
+        res = requests.post(REST_URL,files=fs)
+        if res.status_code == 200 :
+            print("upload : {} is succeeded".format(file_name))
+            task_id = res.json()["task_ids"][0]
+            response_data = status_check(task_id)
+            return response_data
         else :
-            print("{} is failed".format(file_name))
+            print("upload : {} is failed".format(file_name))
+            return None
 
 def run( root , process_count = os.cpu_count() ) :
     file_path = root
     mp.freeze_support()
-    send_file(file_path)
+    response_data = send_file(file_path)
+    return response_data
 
 def status_check(taskid = None):
     if taskid is None or taskid < 1:
         print("Task id is wrong")
         return
     global request
-    api_url = os.path.join(REPORT_URL, str(taskid))
+    api_url = REPORT_URL+ "/" + str(taskid)
     previous_time = time.time()
     while True:
         if  time.time() - previous_time  > 1:
-            response = api_request(api_url)
-            if response.status_code == 200 :
+            res = api_request(api_url)
+            print(res.text)
+            if res.status_code == 200 :
                 break
             previous_time = time.time()
-    jsonreply = json.loads(response.text)
-    print(jsonreply)
+    json_reply = json.loads(res.text)
+    print(json_reply)
+    return json_reply
 
 def api_request(url):
-    response = requests.get(url)
-    return response
+    resp = requests.get(url)
+    return resp
 
 
 if __name__ == '__main__' :
