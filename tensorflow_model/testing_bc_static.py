1d4843b3fb3c094ed5b91c5cfea8c4a1c02d3fe9
Kim Young Jae <corea_kyj@naver.com>
2018-05-14 22:47:14 +0900

    학습된 모델 테스트 하는 코드

---
+++ tensorflow_model/testing_bc_static.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+import numpy as np
+import pickle, warnings, logging, sys
+
+from settings import *
+
+# for using tensorflow as hyper parameter
+INPUT_SIZE = 12288
+OUTPUT_SIZE = 2
+
+RESULT = [ False, True ]
+
+def bc_run( feature_vector ) :
+    warnings.filterwarnings('ignore')
+    logger = logging.getLogger()
+    logger.disabled = True
+    tf.reset_default_graph()
+    with tf.device('/gpu:0'):
+        x = tf.placeholder(tf.float32, shape=[None, INPUT_SIZE])
+        y = tf.placeholder(tf.float32, shape=[None, OUTPUT_SIZE])
+
+        dense_layer_1 = tf.layers.dense(inputs=x, units=4096, activation=tf.nn.relu)
+        dense_layer_2 = tf.layers.dense(inputs=dense_layer_1, units=1024, activation=tf.nn.relu)
+        dense_layer_3 = tf.layers.dense(inputs=dense_layer_2, units=256, activation=tf.nn.relu)
+        dense_layer_4 = tf.layers.dense(inputs=dense_layer_3, units=64, activation=tf.nn.relu)
+        dense_layer_5 = tf.layers.dense(inputs=dense_layer_4, units=16, activation=tf.nn.relu)
+
+        y_ = tf.layers.dense(inputs=dense_layer_5, units=OUTPUT_SIZE)
+        y_test = tf.nn.softmax(y_)
+
+    # testing session start
+    model_saver = tf.train.Saver()
+    init = tf.global_variables_initializer()
+
+    tf_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
+    tf_config.gpu_options.allow_growth = True
+
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
+        sess.run(init)
+        model_saver.restore(sess, STATIC_BC_CHECK_POINT)
+        output = np.array(sess.run(y_test, feed_dict={x: [feature_vector]})).reshape([-1])
+        malware_score = output[-1]
+        detected=RESULT[int(output.argmax(-1))]
+    return detected, malware_score
+
+def load_data( file_path ) :
+    with open(file_path, 'rb') as f :
+        return pickle.load(f)
+
+if __name__ == '__main__' :
+    #fh_path = sys.argv[1]
+    fh_path = "E:\\byeongal\\2018-cap1-16\\tensorflow_model\\train_static_data\\86076973edaa53d60e4853edc384d0f9.fhfops"
+    feature_vector = load_data( fh_path )
+    detected, malware_score = bc_run(feature_vector)
+    print("{},{:.6f}".format(detected, malware_score))
