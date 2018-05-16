350cec23ed405ad835bfca56955f0aa9a919061d
Junnyung-Heo <hurjn96@naver.com>
2018-05-16 16:43:04 +0900

    modify previous ai testing way to new ai testing way

--- web/analysisapp/static_anlysis.py
+++ web/analysisapp/static_anlysis.py
@@ -3,25 +3,29 @@ from django.conf import settings
 
 def run_static_analysis(upload_file_obj):
 
-    IDA_ROOT = os.path.join(settings.PROJECT_DIR,'opseq_ida')
-    ANN_ROOT = os.path.join(settings.PROJECT_DIR, 'ann_by_static')
+    IDA_ROOT = os.path.join(settings.PROJECT_DIR,'ida')
+    FEATURE_ROOT = os.path.join(settings.PROJECT_DIR,'make_feature')
+    TENSOR_ROOT = os.path.join(settings.PROJECT_DIR, 'tensorflow_model')
 
     upload_file_path = os.path.join(settings.MEDIA_ROOT, upload_file_obj.upload_file.name)
 
-    ops_folder_path = os.path.join(IDA_ROOT,'ops')
-    ops_file_path = os.path.join(ops_folder_path,os.path.splitext(upload_file_obj.upload_file.name)[0]+'.ops')
-    fh_folder_path = os.path.join(IDA_ROOT,'fh')
-    fh_file_path = os.path.join(fh_folder_path,os.path.splitext(upload_file_obj.upload_file.name)[0]+'.fh')
-    static_analysis_json_path = os.path.join(os.path.join(ANN_ROOT,'test_result'),os.path.splitext(upload_file_obj.upload_file.name)[0]+'.json')
+    fops_folder_path = os.path.join(IDA_ROOT,'fops')
+    fops_file_path = os.path.join(fops_folder_path,os.path.splitext(upload_file_obj.upload_file.name)[0]+'.fops')
+    fh_fops_folder_path = os.path.join(FEATURE_ROOT,'fh_fops')
+    fh_fops_file_path = os.path.join(fh_fops_folder_path,os.path.splitext(upload_file_obj.upload_file.name)[0]+'.fhfops')
 
-    cmd_run_ida_ops = 'python ' + IDA_ROOT + os.sep + 'make_idb_ops.py ' + upload_file_path
-    cmd_run_ida_fh = 'python ' + IDA_ROOT + os.sep + 'make_fh.py ' + ops_file_path
-    os.system(cmd_run_ida_ops)
-    os.system(cmd_run_ida_fh)
+    cmd_run_ida_fops = 'python ' + IDA_ROOT + os.sep + 'make_idb_fops.py ' + upload_file_path
+    os.system(cmd_run_ida_fops)
 
-    cmd_run_ann = 'python ' + ANN_ROOT + os.sep + 'ann_by_static.py -t ' + fh_file_path
-    os.system(cmd_run_ann)
+    cmd_run_ida_fh_fops = 'python ' + FEATURE_ROOT + os.sep + 'make_fh_fops.py ' + fops_file_path
+    os.system(cmd_run_ida_fh_fops)
 
-    static_analysis_json = open(static_analysis_json_path)
-    ret_data = json.load(static_analysis_json)
+    cmd_run_tensor_bc = 'python ' + TENSOR_ROOT + os.sep + 'testing_bc_static ' + fh_fops_file_path
+    cmd_run_tensor_mc = 'python ' + TENSOR_ROOT + os.sep + 'testing_mc_static ' + fh_fops_file_path
+
+    os.system(cmd_run_tensor_bc)
+    os.system(cmd_run_tensor_mc)
+
+    #static_analysis_json = open(static_analysis_json_path)
+    ret_data = None
     return ret_data
