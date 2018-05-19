37240cce2f79590473ed2bc7276803e5a6ee05fc
Kim Young Jae <corea_kyj@naver.com>
2018-05-19 16:24:22 +0900

    virustotal_public.py 에 필요한 상수 추가

--- aws_automation/settings.py
+++ aws_automation/settings.py
@@ -1,11 +1,24 @@
+# FTP Setting
 FTP_HOST = '203.246.112.137'
 FTP_PORT = 2221
 ID = 'seclab'
 PASSWD = 'seclab4680'
+
+# INSTANCE Number
 INSTANCE_NUM = '1'
 
-remote_raw_path = '/home/seclab/malwares/test/'+INSTANCE_NUM
+# ??
+remote_raw_path = '/home/seclab/malwares/test/'+ INSTANCE_NUM
 remote_zip_path = '/home/seclab/malwares/report/'
-local_report_dir = '/home/ubuntu/report/'
+
+# ??
+LOCAL_REPORT_PATH = '/home/ubuntu/report/'
 local_zip_dir = '/home/ubuntu/zip'
 local_zip_path = '/home/ubuntu/zip/'+INSTANCE_NUM
+
+# Interval Time
+INTERVAL_TIME = 0.05
+
+# Virus Total API Key List
+API_KEY_LIST = [
+]
