dd09fa39b9876a223412dfe032188d5b22184fb9
Junnyung-Heo <hurjn96@naver.com>
2018-05-20 00:04:10 +0900

    리포트 폴더가 없을경우 리포트가 생성되지않는 버그 수정

--- aws_automation/instance_script.py
+++ aws_automation/instance_script.py
@@ -32,6 +32,8 @@ def run():
     md5_list = [ os.path.splitext(file)[0] for file in file_list ]
     key_list = [ API_KEY_LIST[i % len(API_KEY_LIST)] for i in range(len(md5_list))]
     report_path_list = [ LOCAL_REPORT_PATH for i in range(len(md5_list)) ]
+    if not os.path.exists(LOCAL_REPORT_PATH):
+        os.makedirs(LOCAL_REPORT_PATH)
     with multiprocessing.Pool(processes=os.cpu_count()) as pool:
         pool.starmap(vt.retrieving_file_scan_report, zip(md5_list, report_path_list, key_list))
     make_zip( LOCAL_REPORT_PATH,LOCAL_ZIP_PATH)
