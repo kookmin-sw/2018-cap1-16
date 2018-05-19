546683f1193615492a664b0a8d662f331ce7ba7c
Kim Young Jae <corea_kyj@naver.com>
2018-05-19 16:24:53 +0900

    VirusTotal에서 파일 리포트 가져 오는 코드로 수정

--- aws_automation/instance_script.py
+++ aws_automation/instance_script.py
@@ -1,50 +1,43 @@
 import ftplib, os, socket, json, shutil, time
-import settings
+
+import multiprocessing
+
+import virustotal_public as vt
+
+from settings import *
 
 def connect():
-	ftp = ftplib.FTP()
-	ftp.connect(settings.FTP_HOST,settings.FTP_PORT)
-	ftp.login(settings.ID,settings.PASSWD)
-	return ftp
-
-def get_ip_address():
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.connect(("8.8.8.8", 80))
-	return s.getsockname()[0]
+    ftp = ftplib.FTP()
+    ftp.connect(FTP_HOST,FTP_PORT)
+    ftp.login(ID,PASSWD)
+    return ftp
 
 def get_file_list(ftp,remote_dir_path):
-	ftp.cwd(remote_dir_path)
-	return ftp.nlst()
+    ftp.cwd(remote_dir_path)
+    return ftp.nlst()
 
 def upload_files(ftp,local_dir_path,remote_dir_path):
-	ftp.cwd(remote_dir_path)
-	file_list = os.listdir(local_dir_path)
-	for f in file_list:
-		time.sleep(0.05)
-		ftp.storbinary("STOR " + f ,open(os.path.join(local_dir_path,f),'rb'))
+    ftp.cwd(remote_dir_path)
+    file_list = os.listdir(local_dir_path)
+    for f in file_list:
+        time.sleep(INTERVAL_TIME)
+        ftp.storbinary("STOR " + f ,open(os.path.join(local_dir_path,f),'rb'))
 
 def make_zip(dir_path,zip_path):
-	shutil.make_archive(zip_path,'zip',dir_path)
+    shutil.make_archive(zip_path,'zip',dir_path)
 
 def run():
-	ftp = connect()
-	list_file = get_file_list(ftp,settings.remote_raw_path)
-	
-	for f in list_file:
-		report = dict()
-		md5 = os.path.splitext(f)[0]
-		report['ip'] = get_ip_address()
-
-		if not os.path.exists(settings.local_report_dir):
-			os.makedirs(settings.local_report_dir)
-		out_file = open(settings.local_report_dir+md5+'.json','w')
-		json.dump(report,out_file)
-		out_file.close()
-
-	make_zip(settings.local_report_dir,settings.local_zip_path)
-	upload_files(ftp,settings.local_zip_dir,settings.remote_zip_path)
-	shutil.rmtree(settings.local_report_dir)
-	shutil.rmtree(settings.local_zip_dir)
+    ftp = connect()
+    file_list = get_file_list(ftp,remote_raw_path)
+    md5_list = [ os.path.splitext(file)[0] for file in file_list ]
+    key_list = [ API_KEY_LIST[i % len(API_KEY_LIST)] for i in range(len(md5_list))]
+    report_path_list = [ LOCAL_REPORT_PATH for i in range(len(md5_list)) ]
+    with multiprocessing.Pool(processes=os.cpu_count()) as pool:
+        pool.starmap(vt.retrieving_file_scan_report, zip(file_list, report_path_list, key_list))
+    make_zip( LOCAL_REPORT_PATH,local_zip_path)
+    upload_files(ftp,local_zip_dir,remote_zip_path)
+    shutil.rmtree( LOCAL_REPORT_PATH )
+    shutil.rmtree(local_zip_dir)
 
 if __name__ == '__main__':
-	run()
+    run()
