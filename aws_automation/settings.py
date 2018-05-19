e159b93febec483690871227648399df515a7ef9
Junnyung-Heo <hurjn96@naver.com>
2018-05-20 00:24:01 +0900

    저장되는 report_zip파일 이름규칙 추가

--- aws_automation/settings.py
+++ aws_automation/settings.py
@@ -1,3 +1,4 @@
+import datetime
 # FTP Setting
 FTP_HOST = '203.246.112.137'
 FTP_PORT = 2221
@@ -14,7 +15,7 @@ REMOTE_REPORT_PATH = '/home/seclab/malwares/report/'
 # ??
 LOCAL_REPORT_PATH = '/home/ubuntu/report/'
 LOCAL_ZIP_DIR = '/home/ubuntu/zip'
-LOCAL_ZIP_PATH = '/home/ubuntu/zip/'+INSTANCE_NUM
+LOCAL_ZIP_PATH = '/home/ubuntu/zip/'+str(datetime.datetime.now())+'_'+INSTANCE_NUM
 
 # Interval Time
 INTERVAL_TIME = 0.05
