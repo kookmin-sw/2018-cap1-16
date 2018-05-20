174fa8e055c6032248b56ac6ad70ebe09b0a96bc
Junnyung-Heo <hurjn96@naver.com>
2018-05-20 16:43:36 +0900

    ftp 연결 문제 수정

--- aws_automation/instance_script.py
+++ aws_automation/instance_script.py
@@ -27,6 +27,7 @@ def make_zip(dir_path,zip_path):
     shutil.make_archive(zip_path,'zip',dir_path)
 
 def run():
+    start_time =time.time()
     ftp = connect()
     file_list = get_file_list(ftp,REMOTE_FILE_PATH)
     md5_list = [ os.path.splitext(file)[0] for file in file_list ]
@@ -37,9 +38,10 @@ def run():
     with multiprocessing.Pool(processes=os.cpu_count()) as pool:
         pool.starmap(vt.retrieving_file_scan_report, zip(md5_list, report_path_list, key_list))
     make_zip( LOCAL_REPORT_PATH,LOCAL_ZIP_PATH)
+    ftp = connect()
     upload_files(ftp,LOCAL_ZIP_DIR,REMOTE_REPORT_PATH)
     shutil.rmtree( LOCAL_REPORT_PATH )
     shutil.rmtree(LOCAL_ZIP_DIR)
-
+    print(time.time()-start_time)
 if __name__ == '__main__':
     run()
--- aws_automation/settings.py
+++ aws_automation/settings.py
@@ -15,7 +15,7 @@ REMOTE_REPORT_PATH = '/home/seclab/malwares/report/'
 # ??
 LOCAL_REPORT_PATH = '/home/ubuntu/report/'
 LOCAL_ZIP_DIR = '/home/ubuntu/zip'
-LOCAL_ZIP_PATH = '/home/ubuntu/zip/'+str(datetime.datetime.now())+'_'+INSTANCE_NUM
+LOCAL_ZIP_PATH = '/home/ubuntu/zip/'+INSTANCE_NUM
 
 # Interval Time
 INTERVAL_TIME = 0.05
