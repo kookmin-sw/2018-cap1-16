2c49ba9ecf9499cbcc3f6d5b5f024e94a17bf86d
Junnyung-Heo <hurjn96@naver.com>
2018-05-20 17:46:05 +0900

    인스턴스 객체 접근 개선

--- aws_automation/aws_controller.py
+++ aws_automation/aws_controller.py
@@ -1,25 +1,38 @@
-import boto3, time
+import boto3, time, ftplib, os
+from settings import *
 
-ec2 = boto3.client('ec2', region_name='us-west-2', aws_access_key_id='AKIAIDEKND2OCPILXFVQ', aws_secret_access_key='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')
-data = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])['Reservations']
+def connect():
+    ftp = ftplib.FTP()
+    ftp.connect(FTP_HOST,FTP_PORT)
+    ftp.login(ID,PASSWD)
+    return ftp
 
-print(len(data[0]['Instances']))
-'''
-for instance in data[0]['Instances']:
+# 인스턴스 목록 뽑아내기.
+# Filters에 Values [] 안에 정규식을 넣어주면 걸러짐 현재 인스턴스 이름들은 capstone1, capstone2
+ec2 = boto3.client('ec2', region_name='us-west-2',aws_access_key_id='', aws_secret_access_key='')
+instances = ec2.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['capstone*']}])['Reservations'][0]['Instances']
+print(len(instances))
+
+#인스턴스 시작 코드
+for instance in instances:
     print(instance['InstanceId'])
     ec2.start_instances(InstanceIds = [instance['InstanceId']])
-'''
-#time.sleep(300)
-for instance in data[0]['Instances']:
+
+# 인스턴스 키는 시간(100sec) + 스타트업 스크립트 실행시간(2000sec)
+time.sleep(2100)
+
+# 인스턴스 스탑 코드
+for instance in instances:
     print(instance['InstanceId'])
-    ec2.stop_instances(InstanceIds = [instance['InstanceId']])
+    ec2.stop_instances(InstanceIds=[instance['InstanceId']])
 
-#print(ec2.start_instances())
-#instances = ec2.instances.filter(
-#    Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
 
-#for status in ec2.meta.client.describe_instance_status()['InstanceStatuses']:
-#    print(status)
-#
-#for instance in instances:
-#    print(instance.run_instances())
+# zip 파일이 다 모였는지 확인하는 부분 (미완성)
+ftp = connect()
+ftp.cwd(REMOTE_REPORT_PATH)
+zip_list = ftp.nlst()
+zip_num_list = [os.path.splitext(zip)[0] for zip in zip_list]
+if not len(zip_num_list) == 24:
+    required_list = [ str(i) for i in range(1,25)]
+    remain_list = set(required_list) - set(zip_num_list)
+    print(remain_list)
