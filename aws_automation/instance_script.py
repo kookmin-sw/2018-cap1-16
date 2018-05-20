513bef10dbcd1a402a916f64d8368c8a76b8a178
Kim Young Jae <corea_kyj@naver.com>
2018-05-20 16:34:50 +0900

    리포트 완료될때까지 돌도록 수정

--- aws_automation/instance_script.py
+++ aws_automation/instance_script.py
@@ -30,12 +30,19 @@ def run():
     ftp = connect()
     file_list = get_file_list(ftp,REMOTE_FILE_PATH)
     md5_list = [ os.path.splitext(file)[0] for file in file_list ]
-    key_list = [ API_KEY_LIST[i % len(API_KEY_LIST)] for i in range(len(md5_list))]
-    report_path_list = [ LOCAL_REPORT_PATH for i in range(len(md5_list)) ]
-    if not os.path.exists(LOCAL_REPORT_PATH):
-        os.makedirs(LOCAL_REPORT_PATH)
-    with multiprocessing.Pool(processes=os.cpu_count()) as pool:
-        pool.starmap(vt.retrieving_file_scan_report, zip(md5_list, report_path_list, key_list))
+    while len(md5_list) != 0 :
+        key_list = [ API_KEY_LIST[i % len(API_KEY_LIST)] for i in range(len(md5_list))]
+        report_path_list = [ LOCAL_REPORT_PATH for i in range(len(md5_list)) ]
+        if not os.path.exists(LOCAL_REPORT_PATH):
+            os.makedirs(LOCAL_REPORT_PATH)
+
+        with multiprocessing.Pool(processes=os.cpu_count()) as pool:
+            pool.starmap(vt.retrieving_file_scan_report, zip(md5_list, report_path_list, key_list))
+
+        json_list = os.listdir(LOCAL_REPORT_PATH)
+        json_md5_list = [ os.path.splitext(json_path)[0] for json_path in json_list ]
+        md5_list = list( set(md5_list) - set(json_md5_list) )
+
     make_zip( LOCAL_REPORT_PATH,LOCAL_ZIP_PATH)
     upload_files(ftp,LOCAL_ZIP_DIR,REMOTE_REPORT_PATH)
     shutil.rmtree( LOCAL_REPORT_PATH )
