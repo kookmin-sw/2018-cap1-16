a00c75eef265ef16af82080a64e1502a638068e5
Brilliantw <mjw95@kookmin.ac.kr>
2018-05-21 20:08:55 +0900

    make virustotal_report_parser

---
+++ virustotal_labeling/virustotal_report_parser.py
@@ -0,0 +1,65 @@
+import simplejson, os
+
+class Parser :
+    def __init__(self, path) :
+        self.path = path
+        with open(path, 'r') as f :
+            self.json_obj = simplejson.loads(f.read())
+    def md5(self) :
+        try :
+            return self.json_obj['md5']
+        except :
+            return 'no-md5-result'
+    def sha256(self):
+        try :
+            return self.json_obj['sha256']
+        except :
+            return 'no-sha256-result'
+    def sha1(self):
+        try:
+            return self.json_obj['sha1']
+        except :
+            return 'no-sha1-result'
+    def result(self, antivirus_name):
+        try :
+            return self.json_obj['scans'][antivirus_name]['result']
+        except :
+            return 'no-label-result'
+    def detected(self, antivirus_name):
+        try :
+            return self.json_obj['scans'][antivirus_name]['detected']
+        except :
+            return 'no-detected-result'
+    def positives(self):
+        try :
+            return self.json_obj['positives']
+        except :
+            return 'no-positives-result'
+    def total(self):
+        try :
+            return self.json_obj['total']
+        except :
+            return 'no-total-result'
+    def scan_date(self):
+        try :
+            return self.json_obj['scan_date']
+        except :
+            return 'no-scan_date-result'
+    def positive_engine_list(self) :
+        try :
+            ret_list = []
+            for engine, result in self.json_obj['scans'].items() :
+                if result['detected'] :
+                    ret_list.append(engine)
+            return ret_list
+        except :
+            return []
+    def negative_engine_list(self) :
+        try :
+            ret_list = []
+            for engine, result in self.json_obj['scans'].items() :
+                if not result['detected'] :
+                    ret_list.append(engine)
+            return ret_list
+        except :
+            return []
