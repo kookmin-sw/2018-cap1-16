ab5ba8ff68bd37f62c2e6166e49d79cc851e43ec
Kim Young Jae <corea_kyj@naver.com>
2018-05-22 12:14:40 +0900

    Update 2018.05.22
    * MALWARE 폴더에서만 분석 가능하게 수정
    * TIMEOUT 추가
    * 기타 필요없는 코드 제거

--- ida/make_idb_fops.py
+++ ida/make_idb_fops.py
@@ -7,9 +7,9 @@ from settings import *
 
 SCRIPT_PATH = os.path.join(IDA_PYTHON_SCRIPT_PATH, 'fopcode.py')
 
-def create_file_list ( root ) :
+def create_file_list () :
     ret_list = []
-    for path, dirs, files in os.walk(root) :
+    for path, dirs, files in os.walk(MALWARE_PATH) :
         for file in files :
             full_file_path = os.path.join(path, file)
             ret_list.append(full_file_path)
@@ -35,37 +35,35 @@ def make_idb_fops( file_path ) :
 
     command = '"{ida_path}" -c -o"{idb_path}" -A -S"{script_path} {fops_path}" -P+ "{malware_path}"'.format(ida_path=IDA_PATH, idb_path=dst_path, script_path=SCRIPT_PATH, fops_path=fops_dst_path, malware_path=file_path)
     try:
-        subprocess.run(command)
+        subprocess.call(command, shell=True, timeout=TIME_OUT)
         if os.path.exists(dst_path + '.i64') or os.path.exists(dst_path + '.idb'):
             print("{}을 성공적으로 분석하였습니다.".format(file_name))
-            os.remove(file_path)
         else:
             print("{}을 분석하는데 실패하였습니다.".format(file_name))
     except:
         print("{}을 분석하는데 실패하였습니다.".format(file_name))
 
+def print_help() :
+    print("make_idb_fops.py")
+    print("IDA Database 파일과 함수별 Opcode Sequence를 생성해주는 파이썬 스크립트")
+    print("python make_idb_func_fops.py <file> : <file>에 대해 idb(i64) 파일과 fops파일을 추출해 IDB_PATH 와 FOPS_PATH 로 저장")
+
 if __name__ == '__main__' :
-    if not os.path.exists(IDB_PATH) :
-        os.makedirs(IDB_PATH)
-    if not os.path.exists(OPS_PATH) :
-        os.makedirs(OPS_PATH)
     argv_cnt = len(sys.argv)
-    if argv_cnt != 2 :
-        print("make_idb_fops.py")
-        print("IDA Database 파일과 함수별 Opcode Sequence를 생성해주는 파이썬 스크립트")
-        print("python make_idb_func_fops.py <directory> : <directory> 하위에 있는 모든 파일에 대한 idb(i64) 파일과 ffops파일을 추출" )
-        print("python make_idb_func_fops.py <file> : <file>에 대해 idb(i64) 파일과 ffops파일을 추출")
+    if argv_cnt == 1 :
+        mp.freeze_support()
+        file_list = create_file_list()
+        print("Total Malware Count : {}".format(len(file_list)))
+        p = mp.Pool(CPU_COUNT)
+        p.map(make_idb_fops, file_list)
+
     if argv_cnt == 2 :
         path = sys.argv[1]
         if os.path.isfile(path) :
             make_idb_fops(path)
-        elif os.path.isdir(path) :
-            mp.freeze_support()
-            file_list = create_file_list(path)
-            print("Total Malware Count : {}".format(len(file_list)))
-            p = mp.Pool(CPU_COUNT)
-            p.map(make_idb_fops, file_list)
         else :
             print("존재하는 파일 혹은 폴더가 아닙니다.")
+    else :
+        print_help()
 
 
