c1223963b85516a5e7455248b97713c6e370251d
Kim Young Jae <corea_kyj@naver.com>
2018-05-22 12:15:15 +0900

    IDB_PATH 에서만 FOPS파일 만들게 수정 ( 기존 경로넘기는 기능 제거 )

--- ida/make_fops.py
+++ ida/make_fops.py
@@ -6,9 +6,9 @@ from settings import *
 
 SCRIPT_PATH = os.path.join(IDA_PYTHON_SCRIPT_PATH, 'fopcode.py')
 
-def create_idb_list ( root ) :
+def create_idb_list () :
     ret_list = []
-    for path, dirs, files in os.walk(root) :
+    for path, dirs, files in os.walk(IDB_PATH) :
         for file in files :
             ext = os.path.splitext(file)[-1]
             if ext == '.i64' or ext == '.idb' :
@@ -34,19 +34,23 @@ def make_fops( file_path ) :
         return
 
     command = '"{ida_path}" -A -S"{script_path} {fops_path}" "{idb_path}"'.format(ida_path=IDA_PATH, script_path = SCRIPT_PATH, fops_path = fops_dst_path, idb_path=file_path)
+    subprocess.call(command, shell=True)
+    print(file_name)
 
-    subprocess.run(command)
-
+def print_help() :
+    print("make_fops.py")
+    print("IDA Database 파일로 부터 함수 단위 Opcode Sequence를 추출하는 코드")
+    print("python make_fops.py <file> : <file>에서 fops파일을 FOPS_PATH에 생성")
 if __name__ == '__main__' :
-    if not os.path.exists(OPS_PATH) :
-        os.makedirs(OPS_PATH)
     argv_cnt = len(sys.argv)
-    if argv_cnt != 2 :
-        print("make_fops.py")
-        print("IDA Database 파일로 부터 함수 단위 Opcode Sequence를 추출하는 코드")
-        print("python make_fops.py <directory> : <directory> 하위에 있는 모든 idb(i64) 파일로부터 fops파일을 추출" )
-        print("python make_fops.py <file> : <file>에서 fops파일을 추출")
-    if argv_cnt == 2 :
+    if argv_cnt == 1 :
+        mp.freeze_support()
+        file_list = create_idb_list()
+        print("Total IDB Count : {}".format(len(file_list)))
+        p = mp.Pool(CPU_COUNT)
+        p.map(make_fops, file_list)
+
+    elif argv_cnt == 2 :
         path = sys.argv[1]
         if os.path.isfile(path) :
             ext = os.path.splitext(path)[-1]
@@ -54,11 +58,7 @@ if __name__ == '__main__' :
                 make_fops(path)
             else:
                 print("IDB 파일이 아닙니다.")
-        elif os.path.isdir(path) :
-            mp.freeze_support()
-            file_list = create_idb_list(path)
-            print("Total IDB Count : {}".format(len(file_list)))
-            p = mp.Pool(CPU_COUNT)
-            p.map(make_fops, file_list)
         else :
             print("존재하는 파일 혹은 폴더가 아닙니다.")
+    else :
+        print_help()
