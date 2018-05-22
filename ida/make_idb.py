b700dfed02c96ba7cb1c24680f991a91aca4a8b2
Kim Young Jae <corea_kyj@naver.com>
2018-05-22 10:59:15 +0900

    Update 2018.05.22 * 디렉토리를 인자로 넘어오는 기능 수정 ( MALWARE_PATH 에만 있게 ) * 설명 변경 * 작업 다 끝난 다음 파일 지우게 수정 * 필요 없는 코드 제거 ( OPS 관련 ) * IDA 시간 제한 추가 ( TIME_OUT 변수 단위 s )

--- ida/make_idb.py
+++ ida/make_idb.py
@@ -5,11 +5,9 @@ import sys
 
 from settings import *
 
-SCRIPT_PATH = os.path.join(IDA_PYTHON_SCRIPT_PATH, 'opcode.py')
-
-def create_file_list ( root ) :
+def create_file_list () :
     ret_list = []
-    for path, dirs, files in os.walk(root) :
+    for path, dirs, files in os.walk(MALWARE_PATH) :
         for file in files :
             full_file_path = os.path.join(path, file)
             ret_list.append(full_file_path)
@@ -27,44 +25,61 @@ def make_idb( file_path ) :
     dst_path = os.path.join(idb_save_path, file_name)
     if os.path.exists(dst_path + '.i64') or os.path.exists(dst_path + '.idb') :
         print("{}는 이미 분석결과가 존재 합니다.".format(file_name))
-        os.remove(file_path)
         return
 
     command = '"{ida_path}" -c -o"{idb_path}" -B -P+ "{malware_path}"'.format(ida_path=IDA_PATH, idb_path=dst_path, malware_path=file_path)
+
     try :
-        subprocess.run(command)
+        subprocess.call(command, shell=True, timeout=TIME_OUT)
         if os.path.exists(dst_path + '.i64') or os.path.exists(dst_path + '.idb') :
             print("{}을 성공적으로 분석하였습니다.".format(file_name))
-            #os.remove(dst_path + '.asm')
-            #os.remove(file_path)
+            os.remove(dst_path + '.asm')
         else :
             print("{}을 분석하는데 실패하였습니다.".format(file_name))
     except :
         print("{}을 분석하는데 실패하였습니다.".format(file_name))
 
+def create_delete_vir_list() :
+    ret_list = []
+    for path, dirs, files in os.walk(IDB_PATH) :
+        for file in files :
+            ext = os.path.splitext(file)[-1]
+            if ext == '.i64' :
+                ret_list.append(os.path.join(path, file.replace('.i64', '.vir')))
+            elif ext == '.idb' :
+                ret_list.append(os.path.join(path, file.replace('.idb', '.vir')))
+    return ret_list
+
+def delete_file( file_path ) :
+    os.remove(file_path)
+
+def print_help() :
+    print("make_idb.py")
+    print("IDA Database 을 생성해주는 파이썬 스크립트")
+    print("python make_idb.py <file> : <file>에 대해 idb(i64) 파일을 IDB_PATH에 생성")
+
 if __name__ == '__main__' :
     if not os.path.exists(IDB_PATH) :
         os.makedirs(IDB_PATH)
-    if not os.path.exists(OPS_PATH) :
-        os.makedirs(OPS_PATH)
     argv_cnt = len(sys.argv)
-    if argv_cnt != 2 :
-        print("make_idb.py")
-        print("IDA Database 을 생성해주는 파이썬 스크립트")
-        print("python make_idb.py <directory> : <directory> 하위에 있는 모든 파일에 대한 idb(i64) 파일 생성" )
-        print("python make_idb.py <file> : <file>에 대해 idb(i64) 파일 생성")
+    if argv_cnt == 1 :
+        mp.freeze_support()
+        file_list = create_file_list()
+        file_list.sort()
+        print("Total Malware Count : {}".format(len(file_list)))
+        p = mp.Pool(CPU_COUNT)
+        p.map(make_idb, file_list)
+        print("Delete Process Start")
+        vir_list = create_delete_vir_list()
+        p.map(delete_file, vir_list)
+        print("Done")
     if argv_cnt == 2 :
         path = sys.argv[1]
         if os.path.isfile(path) :
             make_idb(path)
-        elif os.path.isdir(path) :
-            mp.freeze_support()
-            file_list = create_file_list(path)
-            print("Total Malware Count : {}".format(len(file_list)))
-            p = mp.Pool(CPU_COUNT)
-            p.map(make_idb, file_list)
-            print("Done")			
         else :
-            print("존재하는 파일 혹은 폴더가 아닙니다.")
+            print("존재하지 않는 파일입니다.")
+    else :
+        print_help()
 
 
