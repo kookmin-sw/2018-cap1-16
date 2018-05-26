3588965f7b61afb81b3d693e0aba79c010873764
Kim Young Jae <corea_kyj@naver.com>
2018-05-26 15:55:55 +0900

    section 정보 추가하는 코드 추가

--- pefile_viewer/peview.py
+++ pefile_viewer/peview.py
@@ -34,4 +34,26 @@ class Peview :
                     array.append(item[0])
         return array
 
+    def get_sections_info(self) :
+        array = []
+        for section in self.__pe.sections:
+            section.get_entropy()
+            if section.SizeOfRawData == 0 or (
+                    section.get_entropy() > 0 and section.get_entropy() < 1) or section.get_entropy() > 7:
+                suspicious = True
+            else:
+                suspicious = False
+
+            scn = section.Name
+            md5 = section.get_hash_md5()
+            sha1 = section.get_hash_sha1()
+            spc = suspicious
+            va = hex(section.VirtualAddress)
+            vs = hex(section.Misc_VirtualSize)
+            srd = section.SizeOfRawData
+
+            array.append({"name": scn, "hash_md5": md5, "hash_sha1": sha1, "suspicious": spc, "virtual_address": va,
+                          "virtual_size": vs, "size_raw_data": srd})
+
+        return array
 
