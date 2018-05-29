11716f79316526cd83d6c80df1fca4e6eb625687
Kim Young Jae <corea_kyj@naver.com>
2018-05-29 20:33:03 +0900

    pefile 크롤링 도구 설정값 명시

--- pefile_crawling/settings.py
+++ pefile_crawling/settings.py
@@ -1,14 +1,14 @@
-# CPU 개수
-CPU_COUNT = 1
+# CPU COUNT ( os.cpu_count() 미만으로 설정 )
+CPU_COUNT = "사용할 CPU 코어 갯수"
 
 # 32bit exe 파일 옮길 위치
-DESTINATION_32_EXE_PATH = "D:\\exe32"
+DESTINATION_32_EXE_PATH = "32bit exe 파일 옮길 위치"
 
 # 64bit exe 파일 옮길 위치
-DESTINATION_64_EXE_PATH = "D:\\exe64"
+DESTINATION_64_EXE_PATH = "64bit exe 파일 옮길 위치"
 
 # 32bit dll 파일 옮길 위치
-DESTINATION_32_DLL_PATH = "D:\\dll32"
+DESTINATION_32_DLL_PATH = "32bit dll 파일 옮길 위치"
 
 # 64bit dll 파일 옮길 위치
-DESTINATION_64_DLL_PATH = "D:\\dll64"
+DESTINATION_64_DLL_PATH = "64bit dll 파일 옮길 위치"
