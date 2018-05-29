a47ee4fda55d85a3949933fe714fe66aae05d952
Kim Young Jae <corea_kyj@naver.com>
2018-05-29 19:51:03 +0900

    Update 2018.05.29 * 필요 없는 셋팅 값 정리 및 주석 추가

--- aws_automation/settings.py
+++ aws_automation/settings.py
@@ -1,21 +1,24 @@
-import datetime
+import os
+
 # FTP Setting
-FTP_HOST = '203.246.112.137'
-FTP_PORT = 2221
-ID = 'seclab'
-PASSWD = 'seclab4680'
+FTP_HOST = 'YOUR_FTP_HOST(IP)'
+FTP_PORT = 'FTP_PORT(21)'
+ID = 'FTP_ID'
+PASSWD = 'FTP_PASSWORD'
 
-# INSTANCE Number
-INSTANCE_NUM = '1'
+# INSTANCE Number ( 반드시 문자열 구조 ex '1' )
+INSTANCE_NUM = 'INSTANCE_NUM'
 
-# ??
-REMOTE_FILE_PATH = '/home/seclab/malwares/ftp_base/'+ INSTANCE_NUM
-REMOTE_REPORT_PATH = '/home/seclab/malwares/report_zip/'
+# Virus Total 분석이 필요한 파일이 저장되어 있는 경로
+REMOTE_FILE_BASE_PATH = 'FTP_BASE_PATH'
+REMOTE_FILE_PATH = os.path.join(REMOTE_FILE_BASE_PATH, INSTANCE_NUM)
 
-# ??
-LOCAL_REPORT_PATH = '/home/ubuntu/report/'
-LOCAL_ZIP_DIR = '/home/ubuntu/zip'
-LOCAL_ZIP_PATH = '/home/ubuntu/zip'
+# 리포트를 저장할 FTP 위치
+REMOTE_REPORT_PATH = 'REMOTE_REPORT_PATH'
+
+# 인스턴스에서 리포트 및 리포트를 압축한 파일을 저장할 위치
+LOCAL_REPORT_PATH = 'LOCAL_REPORT_PATH'
+LOCAL_ZIP_DIR = 'LOCAL_REPORT_ZIP_PATH'
 
 # Interval Time
 INTERVAL_TIME = 0.05
