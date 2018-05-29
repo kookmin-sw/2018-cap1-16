d17de9ae5e143847d7f22bf9244fa66861716686
cyhan1004 <cyhan1004@naver.com>
2018-05-29 20:53:41 +0900

    api sequence 추출 스크립트 변경

--- cuckoo/scripts/extractapiseq.py
+++ cuckoo/scripts/extractapiseq.py
@@ -3,40 +3,35 @@ import json
 import sys, os
 import pickle
 
-REPORT = "/home/seclab/170728_report"
-PICKLE = "/home/seclab/170728_acs"
+DIRECTORY = "/home/seclab/.cuckoo/storage/analyses/"
 
-
-def apiExtract(file_path = REPORT):
+def api_extract(dir, file_cnt,DIRECTORY):
     total_cnt = 0
-    reports = os.listdir(file_path)
-    
-    md5_txt_file = open(os.path.join(PICKLE,'md5_170728.txt'),'w')
-    for r in reports :
-        api_list = []
-        report = os.path.join(file_path, r)
-        with open(report) as f:
+    task_list = os.listdir(DIRECTORY)
+    for task in range(1, int(file_cnt)+1):
+        api_list  = []
+        task_dir_name = DIRECTORY + str(task) + "/reports/"
+        task_path = task_dir_name + "report.json"
+        with open(task_path) as f:
             json_data = json.load(f)
             md5 = json_data["target"]["file"]["md5"]
             try:
                 processes = json_data["behavior"]["processes"]
-            except:
-                pass
-            try:
                 for i in range(0, len(processes)):
                     if processes[i]["track"] == True:
                         for j in range(0, len(processes[i]["calls"])):
                             api_list.append(processes[i]["calls"][j]["api"])
             except:
                 pass
-            if(len(api_list)>=5):
-                pickle_dir = os.path.join(PICKLE, md5+".acs")
+            if (len(api_list) >= 5):
+                pickle_dir = os.path.join(dir, md5 + ".acs")
                 with open(pickle_dir, 'wb') as p:
                     pickle.dump(api_list, p)
-                md5_txt_file.write(md5+'\n')
                 total_cnt += 1
-                
-    md5_txt_file.close()
+
             
 if __name__ == '__main__':
-    apiExtract() 
+    pickle_dir = sys.argv[1]
+    cnt = sys.argv[2]
+    cuckoo_dir = sys.argv[3]
+    api_extract(pickle_dir,cnt,cuckoo_dir)
