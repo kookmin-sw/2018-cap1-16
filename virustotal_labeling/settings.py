16c9f407bee543bae52b7fbfd665c64c32c4d704
Kim Young Jae <corea_kyj@naver.com>
2018-05-29 20:36:24 +0900

    CSV 값 만드는 코드 설정값 명시

--- virustotal_labeling/settings.py
+++ virustotal_labeling/settings.py
@@ -1,7 +1,5 @@
-import os
-
 # json 경로
-JSON_PATH = os.path.normpath(os.path.abspath('./json'))
+JSON_PATH = "JSON 파일이 저장되어 있는 경로"
 
 # 정보를 가져올 안티바이러스 이름
-ANTIVIRUS_NAME = 'Kaspersky'
+ANTIVIRUS_NAME = "라벨에 사용할 안티바이러스 제품명 ( VT에 있는 것 )"
