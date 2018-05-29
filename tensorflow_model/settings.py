a78e609c1d66e971dd1849d6660b62ce46c5b051
Kim Young Jae <corea_kyj@naver.com>
2018-05-29 20:35:41 +0900

    딥러닝 관련 설정값 명시

--- tensorflow_model/settings.py
+++ tensorflow_model/settings.py
@@ -1,24 +1,21 @@
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 # 정적 이진 Check Point 위치
-STATIC_BC_CHECK_POINT = os.path.join(BASE_DIR,'static_bc_model.ckpt')
+STATIC_BC_CHECK_POINT = "정적 이진 분류 모델 체크포인트 위치"
 
 # 정적 멀티 Check Point 위치
-STATIC_MC_CHECK_POINT = os.path.join(BASE_DIR,'static_mc_model.ckpt')
+STATIC_MC_CHECK_POINT = "정적 다중 분류 모델 체크포인트 위치"
 
 # 정적 분석 학습 파일 위치
-TRAIN_STATIC_DATA_PATH = os.path.normpath(os.path.abspath('./train_static_data'))
+TRAIN_STATIC_DATA_PATH = "정적 피쳐 벡터(fhfops)가 저장되어 있는 경로"
 
 # 동적 이진 Check Point 위치
-DYNAMIC_BC_CHECK_POINT = os.path.join(BASE_DIR,'dynamic_model.ckpt')
+DYNAMIC_BC_CHECK_POINT = "동적 이진 분류 모델 체크포인트 위치"
 
 # 동적 멀티 Check Point 위치
-DYNAMIC_MC_CHECK_POINT = os.path.join(BASE_DIR,'dynamic_mc_model.ckpt')
+DYNAMIC_MC_CHECK_POINT = "동적 이진 분류 모델 체크포인트 위치"
 
 # 동적 분석 학습 파일 위치
-TRAIN_DYNAMIC_DATA_PATH = os.path.normpath(os.path.abspath('./train_dynamic_data'))
+TRAIN_DYNAMIC_DATA_PATH = "동적 피쳐 벡터(fhacs)가 저장되어 있는 경로"
 
 # 학습에 사용할 라벨
-TRAIN_LABEL_PATH = os.path.normpath(os.path.abspath('./train_data.csv'))
+TRAIN_LABEL_PATH = "학습에 사용될 csv(md5, detected, label)이 저장되어 있는 경로"
 
